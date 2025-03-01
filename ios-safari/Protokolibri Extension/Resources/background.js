11e8da0c806c9653bbe97e2fd958be283b4252dc
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2025-03-01 14:46:58 +0100

    webNavigation listener

--- ios-safari/Protokolibri Extension/Resources/background.js
+++ ios-safari/Protokolibri Extension/Resources/background.js
@@ -1,6 +1,11 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+  console.log("Received request: ", request);
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
+  if (request.greeting === "hello") sendResponse({ farewell: "goodbye" });
 });
+
+browser.webNavigation.onCommitted.addListener((details) => {
+  console.log(details);
+});
+
+console.log("Protokolibri background.js started");
