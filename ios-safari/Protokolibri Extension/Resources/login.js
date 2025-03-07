8216063dbb168a19a1f69dba6a84b6e96d3d14d6
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2025-03-07 12:56:11 +0100

    login error handling (distiction between wrong credetials and server error)

--- ios-safari/Protokolibri Extension/Resources/login.js
+++ ios-safari/Protokolibri Extension/Resources/login.js
@@ -8,18 +8,37 @@ window.addEventListener('load', () => {
     elm.innerHTML = t(elm.getAttribute('data-i18n'));
   });
   document.querySelector('#retry').addEventListener('click', login);
+  document
+    .querySelector('#close')
+    .addEventListener('click', async () =>
+      browser.tabs.remove((await browser.tabs.getCurrent()).id)
+    );
 
   // wait for the dom to update
   setTimeout(login);
 });
 
-const login = async () => {
-  const box = document.querySelector('#statusBox');
-  const status = document.querySelector('#status');
-  const message = document.querySelector('#message');
-  const retry = document.querySelector('#retry');
-  retry.classList.add('hidden');
+const setStatus = (success, message, retry = false) => {
+  document.querySelector('#statusBox').classList.remove('hidden');
+  document.querySelector('#status').innerHTML = success
+    ? t('login_status_success')
+    : t('login_status_failed');
+  document.querySelector('#message').innerHTML = message;
+  document.querySelector('#retry').classList.toggle('hidden', !retry);
+  document.querySelector('#close').classList.toggle('hidden', !success);
+};
 
+let confirmed = false;
+
+const login = async () => {
+  document.querySelector('#statusBox').classList.add('hidden');
+
+  // wait for the dom to update
+  const { promise, resolve } = Promise.withResolvers();
+  setTimeout(resolve, 1);
+  await promise;
+
+  // extract login data from get parameters
   const params = new URLSearchParams(document.location.search);
   const url = URL.parse(params.get('url'));
   const user = params.get('user');
@@ -36,9 +55,7 @@ const login = async () => {
     !/^[\x21-\x7e]+$/.test(password) ||
     !/^[a-zA-Z0-9_\-\.]+$/.test(device)
   ) {
-    box.classList.remove('hidden');
-    status.innerHTML = t('login_status_failed');
-    message.innerHTML = t('login_message_bad_data');
+    setStatus(false, t('login_message_bad_data'));
     return;
   }
 
@@ -47,13 +64,11 @@ const login = async () => {
   url.search = '';
 
   // user confirms login
-  if (!confirm(t('login_confirm', [url.href, user, device]))) {
-    box.classList.remove('hidden');
-    status.innerHTML = t('login_status_failed');
-    message.innerHTML = t('login_message_canceled');
-    retry.classList.remove('hidden');
+  if (!confirmed && !confirm(t('login_confirm', [url.href, user, device]))) {
+    setStatus(false, t('login_message_canceled'), true);
     return;
   }
+  confirmed = true;
 
   // check login data against server
   try {
@@ -61,17 +76,19 @@ const login = async () => {
       credentials: 'include',
       headers: { Authorization: `Basic ${btoa(`${user}:${password}`)}` },
     });
-    if (!res.ok) {
-      box.classList.remove('hidden');
-      status.innerHTML = t('login_status_failed');
-      message.innerHTML = t('login_message_unauthorized');
+    if (res.status === 401) {
+      setStatus(false, t('login_message_unauthorized'));
+      return;
+    } else if (!res.ok) {
+      setStatus(
+        false,
+        t('login_message_server_error', [res.status, res.statusText]),
+        true
+      );
       return;
     }
-  } catch {
-    box.classList.remove('hidden');
-    status.innerHTML = t('login_status_failed');
-    message.innerHTML = t('login_message_network_error');
-    retry.classList.remove('hidden');
+  } catch (e) {
+    setStatus(false, t('login_message_network_error', e.toString()), true);
     return;
   }
 
@@ -80,16 +97,11 @@ const login = async () => {
     await browser.storage.local.set({
       server: { url: url.href, user, password, device },
     });
-  } catch {
-    box.classList.remove('hidden');
-    status.innerHTML = t('login_status_failed');
-    message.innerHTML = t('login_message_internal_error');
-    retry.classList.remove('hidden');
+  } catch (e) {
+    setStatus(false, t('login_message_internal_error', e.toString()), true);
     return;
   }
 
   // success message
-  box.classList.remove('hidden');
-  status.innerHTML = t('login_status_success');
-  message.innerHTML = t('login_message_success', [url.href, user, device]);
+  setStatus(true, t('login_message_success', [url.href, user, device]));
 };
